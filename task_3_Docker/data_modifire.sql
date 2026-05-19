115aa4c2fc63b3dc263bae0856c03b8c233bed33
mironovak <ksyusha.mironova.2004@mail.ru>
2026-05-19 23:14:05 +0500

    task 8 добавлено

--- task_3_Docker/data_modifire.sql
+++ task_3_Docker/data_modifire.sql
@@ -4,3 +4,19 @@ UPDATE user_logs SET s_all_avg = REPLACE(s_all_avg, ',', '.') WHERE s_all_avg LI
 ALTER TABLE user_logs ALTER COLUMN s_all_avg TYPE REAL USING s_all_avg::REAL;
 
 SELECT AVG(s_all_avg) AS average_activity FROM user_logs;
+
+
+
+LTER TABLE public.user_logs ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.user_logs ALTER COLUMN namer_level TYPE int USING namer_level::int;
+ALTER TABLE public.user_logs ALTER COLUMN date_vatt TYPE varchar(255) USING date_vatt::varchar(255);
+
+ALTER TABLE public.user_logs ALTER COLUMN s_q_attempt_viewed_avg TYPE int4 USING s_q_attempt_viewed_avg::int4;
+
+
+
+select d."name",  COUNT(DISTINCT u.courseid) as course_count
+FROM user_logs u
+JOIN departments d ON u.depart = d.id
+GROUP BY d.name
+ORDER BY course_count desc
